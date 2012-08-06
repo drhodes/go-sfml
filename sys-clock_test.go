9346109faddc2f1d850939cbbcee7c849bc11627
Quentin Stievenart <quentin.stievenart@gmail.com>
2012-08-06 17:17:00 +0200

    Improve test of clocks

--- sys-clock_test.go
+++ sys-clock_test.go
@@ -1,31 +1,17 @@
 package sfml
 
 import (
-	"log"
 	"testing"
 )
 
-func TestConstructors(t *testing.T) {
+func TestClockConstructors(t *testing.T) {
 	clock := NewClock()
 	clock.Restart()
-}
-
-func BenchmarkGetTime(b *testing.B) {
-}
-
-func BenchmarkClockDestroy(b *testing.B) {	
-}
-
-func TestMicroseconds(t *testing.T) {
-}
-
-func TestAsSeconds(t *testing.T) {
 }
 
 func TestAsMilliseconds(t *testing.T) {
 	sec := Seconds(123)
 	if sec.AsMilliseconds() != 123000 {
-		log.Println(sec)
 		t.Fail()
 	}
 }
@@ -37,27 +23,9 @@ func TestAsMicroseconds(t *testing.T) {
 	}
 }
 
-func TestSeconds(t *testing.T) {
+func TestAsSeconds(t *testing.T) {
 	sec := Seconds(123)
 	if sec.AsSeconds() != 123 {
 		t.Fail()
 	}
 }
-
-func TestMilliseconds(t *testing.T) {
-}
-
-/*
-func BenchmarkCollidePoly(b *testing.B) {
-	pg1 := new(Polygon)
-	pg2 := new(Polygon)
-	for i := 0; i < b.N; i++ {
-		pg1 = pg1.RandomEl()
-		pg2 = pg2.RandomEl()
-		//Debug(pg1)
-		//Debug(pg2)
-		pg1.Collide(pg2)
-		//Debug("-------------------------------------------")
-	}
-}
-*/
